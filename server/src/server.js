5c8b641f15cb68cbcd51a4ec3d8c4ad31c108f05
josh098741 <morongemokaya@gmail.com>
2025-10-07 23:45:24 +0300

    Additiona commits to the backend after creation of server

---
+++ server/src/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+
+const app = express()
+
+
+app.get("/api/auth/signup",(req,res) => {
+    res.send("Signup endpoint")
+})
+
+app.get("/api/auth/login",(req,res) => {
+    res.send("Login Endpoint")
+})
+
+app.get("/api/auth/logout",(req,res) => {
+    res.send("Logout Endpoint")
+})
+
+const PORT = process.env.PORT || 5000
+
+const start = async () => {
+    try{
+        app.listen(PORT, () => {
+            console.log(`Server is running succesfully on PORT : ${PORT}`)
+        })
+    }catch(error){
+        console.log("There was an error in running the server")
+    }
+}
+
+start()
